b985b66a0ff510117e1e5ecbfbaa7559efb095b2
XGit3000 <sebastian.carrier@gmail.com>
2023-07-06 20:25:39 +0200

    test2

---
+++ test_yeah_u_ifr.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("2o2")
+	print("test from phone")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
