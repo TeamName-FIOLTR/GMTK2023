8b0f21f7ad55b02b5ad10df38a07ba085d90178a
mequam <blue9ja@gmail.com>
2023-07-09 09:11:42 -0400

    updated reset libary cards

--- scenes/objects/cards/reset_library/reset_library_card.gd
+++ scenes/objects/cards/reset_library/reset_library_card.gd
@@ -4,5 +4,5 @@ class_name ResetLibraryCard
 
 
 func play_effect(_decks : RotaryDeck,_entity : Entity)->void:
-	super.play_effect(_decks ,_entity)
 	_decks.reset_library()
+	super.play_effect(_decks ,_entity)
